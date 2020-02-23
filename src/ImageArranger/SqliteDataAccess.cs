d65689128b2925bdb40eb7e89ba53fd6574d977f
Jarad Gray <jarad.h.gray@gmail.com>
2020-02-23 16:16:07 -0600

    implement database access (untested)

---
+++ src/ImageArranger/SqliteDataAccess.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using Dapper;
+
+namespace ImageArranger
+{
+    public class SqliteDataAccess
+    {
+        // API methods
+
+        public static ObservableCollection<FileTimestamp> GetAllTimestamps()
+        {
+            // Safely open a connection to our database
+            using (IDbConnection dbConnection = new SQLiteConnection(LoadConnectionString()))
+            {
+                // Query the database
+                string query = "SELECT * FROM FileTimestamp";
+                var output = dbConnection.Query<FileTimestamp>(query, new DynamicParameters());
+                // Convert output to an ObservableCollection and return it
+                return new ObservableCollection<FileTimestamp>(output);
+            }
+        }
+
+        public static void SaveFileTimestamp(FileTimestamp timestamp)
+        {
+            // Safely open a connection to our database
+            using (IDbConnection dbConnection = new SQLiteConnection(LoadConnectionString()))
+            {
+                // TODO Create a record in FileTimestamp table to represent the given timestamp
+            }
+        }
+
+
+        // Private methods
+
+        /// <summary>
+        /// Returns the ConnectionString attribute of the connectoinString
+        /// in App.config whose name property matches the given id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
